0a7dc14463004fa30f4961bf6718716858d0f95b
Craig <craig.porter7@hotmail.com>
2026-06-15 11:19:10 +0100

    Set base path for Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/gas-suite/',
+  base: '/gassuite/',
   plugins: [react(), tailwindcss()],
 })
